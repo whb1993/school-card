3bbe2d3cfd3c112787bd11e0884ff3a7f306592d
wanghongbin <972815205@qq.com>
2019-07-31 11:39:00 +0800

    init

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'student',
+                loadChildren: './student/student.module#SchoolCardStudentModule'
+            },
+            {
+                path: 'teacher',
+                loadChildren: './teacher/teacher.module#SchoolCardTeacherModule'
+            },
+            {
+                path: 'admin',
+                loadChildren: './admin/vAdmin.module#SchoolCardAdminModule'
+            },
+            {
+                path: 'card',
+                loadChildren: './card/card.module#SchoolCardCardModule'
+            },
+            {
+                path: 'fill-inf',
+                loadChildren: './fill-inf/fill-inf.module#SchoolCardFillInfModule'
+            },
+            {
+                path: 'lost-inf',
+                loadChildren: './lost-inf/lost-inf.module#SchoolCardLostInfModule'
+            },
+            {
+                path: 'press-inf',
+                loadChildren: './press-inf/press-inf.module#SchoolCardPressInfModule'
+            },
+            {
+                path: 'library-inf',
+                loadChildren: './library-inf/library-inf.module#SchoolCardLibraryInfModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
